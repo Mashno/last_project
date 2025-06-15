55e0f2d4156a5f9944adcde340b88586fe2b880c
Mashno <118561914+Mashno@users.noreply.github.com>
2025-06-15 09:43:35 +0300

    first commit

---
+++ Text_analyse/src/main/java/com/mycompany/text_analyse/Text_analyse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.text_analyse;
+
+/**
+ *
+ * @author Владислав
+ */
+public class Text_analyse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
